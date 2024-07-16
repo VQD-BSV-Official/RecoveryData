ac25d41e2b521f1236405aaccc55eca81936e14f
VQD_BSV_Official <QuangDaiVQD@gmail.com>
2024-07-16 19:14:04 +0700

    Add notes

--- Run/Scan_CR2.py
+++ Run/Scan_CR2.py
@@ -1,34 +1,36 @@
 def Scan_CR2_Image(image):
-    markers = [b"\x49\x49\x2A\x00\x10\x00\x00\x00\x43\x52\x02\x00"]
-    mid_marker = b"\xFF\xD8\xFF\xC4"
-    end_marker = b"\xFF\xD9"
-
-    #Scan file
+    # Read byte image
     with open(image, "rb") as file:
         data = file.read()
 
-        count = 0
-        for marker in markers:
-            start_offset = data.find(marker)
+    # 3 marker (s = start, m = mid, e = end)
+    s_marker = b"\x49\x49\x2A\x00\x10\x00\x00\x00\x43\x52\x02\x00"
+    m_marker = b"\xFF\xD8\xFF\xC4"
+    e_marker = b"\xFF\xD9"
 
-            while start_offset != -1:
-                middle_offset = data.find(mid_marker, start_offset)
-              
-                if middle_offset != -1:
-                    end_offset = data.find(end_marker, middle_offset)
-                    if end_offset != -1:
-                        count += 1
-                        # Save
+    # 3 offset (s = start, m = mid, e = end)
+    # count & find s_marker 
+    count = 0
+    s_offset = data.find(s_marker)
 
-                        with open(f"IMG_{count}.CR2", "wb") as w:
-                            if count == 1:
-                                print(f" _IMG_{count}.CR2")
-                            else:
-                                print(f"|_IMG_{count}.CR2")
-                            w.write(data[start_offset:end_offset + len(end_marker)])
+    while s_offset != -1:
+        # Find m_marker 
+        m_offset = data.find(m_marker, s_offset)
+      
+        if m_offset != -1:
+            # Find e_marker
+            e_offset = data.find(e_marker, m_offset)
 
-                        start_offset = data.find(marker, end_offset)
-                    else:
-                        break
-                else:
-                    break
+            if e_offset != -1:
+                count += 1
+
+                # Save
+                with open(f"IMG_{count}.CR2", "wb") as w:
+                    w.write(data[s_offset:e_offset + len(e_marker)]) # len = 2 byte FF D9
+                
+                print(f"|_IMG_{count}.CR2")
+                s_offset = data.find(s_marker, e_offset)
+            else:
+                break
+        else:
+            break
